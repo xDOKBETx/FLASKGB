bacc4e1a548b19fe1fc825fc24963d636f7176b3
Aleksey Valentinov <ds83351@chebnet.com>
2023-08-19 22:41:06 +0300

    changes made to homework from HW_5

--- HW_5/homework/main.py
+++ HW_5/homework/main.py
@@ -1,91 +1,75 @@
-from flask import Flask, request, jsonify, render_template
-from dataclasses import dataclass
-from typing import List
+from fastapi import FastAPI, Request, HTTPException
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from passlib.context import CryptContext
+from pydantic import BaseModel
+from starlette.staticfiles import StaticFiles
+import uvicorn
 
-app = Flask(__name__)
+app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+# Модель данных для ввода нового пользователя
 
 
-@dataclass
-class User:
-    id: int
+class UserIn(BaseModel):
     name: str
     email: str
     password: str
 
-
-users: List[User] = []
-
-
-def is_valid_email(email):
-    # Простая проверка на валидность email, можно использовать более сложные методы
-    return '@' in email
+# Модель данных для вывода пользователя
 
 
-@app.route('/add_user', methods=['POST'])
-def add_user():
-    try:
-        data = request.get_json()
+class User(BaseModel):
+    id: int
+    name: str
+    email: str
 
-        if not data or 'name' not in data or 'email' not in data or 'password' not in data:
-            return jsonify({'error': 'Missing required fields'}), 400
-
-        new_id = len(users) + 1
-        new_user = User(
-            id=new_id, name=data['name'], email=data['email'], password=data['password'])
-        users.append(new_user)
-
-        return jsonify({'message': 'User added successfully'}), 201
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+# Модель данных для хранения пользователя с хешированным паролем
 
 
-@app.route('/update_user/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    try:
-        data = request.get_json()
-
-        if not data or 'name' not in data or 'email' not in data or 'password' not in data:
-            return jsonify({'error': 'Missing required fields'}), 400
-
-        user_to_update = next(
-            (user for user in users if user.id == user_id), None)
-
-        if user_to_update is None:
-            return jsonify({'error': 'User not found'}), 404
-
-        user_to_update.name = data['name']
-        user_to_update.email = data['email']
-        user_to_update.password = data['password']
-
-        return jsonify({'message': 'User updated successfully'}), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+class UserDB(UserIn):
+    hashed_password: str
 
 
-@app.route('/delete_user/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    try:
-        user_to_delete = next(
-            (user for user in users if user.id == user_id), None)
+users = []
 
-        if user_to_delete is None:
-            return jsonify({'error': 'User not found'}), 404
-
-        users.remove(user_to_delete)
-
-        return jsonify({'message': 'User deleted successfully'}), 200
-
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+# Функция для хеширования пароля
 
 
-@app.route('/users', methods=['GET'])
-def user_list():
-    return render_template('user_list.html', users=users)
+def get_password_hash(password):
+    return pwd_context.hash(password)
+
+# Функция для проверки пароля
 
 
+def verify_password(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+# Маршрут для добавления нового пользователя
+
+
+@app.post('/add_user')
+def add_user(user: UserIn):
+    for u in users:
+        if u.email == user.email:
+            raise HTTPException(
+                status_code=400, detail="Email already registered")
+    hashed_password = get_password_hash(user.password)
+    new_user = UserDB(**user.dict(), hashed_password=hashed_password)
+    users.append(new_user)
+    return {"message": "User added successfully"}
+
+# Маршрут для вывода списка пользователей в виде HTML
+
+
+@app.get('/users', response_class=HTMLResponse)
+async def user_list(request: Request):
+    return templates.TemplateResponse('user_list.html', {'request': request, 'users': users})
+
+# Запуск приложения
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
+    uvicorn.run(app, host="127.0.0.1", port=8000)
